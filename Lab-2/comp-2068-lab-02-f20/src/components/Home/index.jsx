bc97774c23fd99db9eab89175b9887aad821f34e
LavishT <lavishtalwar28@github.com>
2020-10-22 20:20:18 -0400

    init

---
+++ Lab-2/comp-2068-lab-02-f20/src/components/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../shared/Header'
+
+// You need to import your shared header...
+
+const Home = () => {
+  return (
+    <Header title="Welcome Home"/>
+  );
+}
+ 
+export default Home;
---
+++ Lab-2/comp-2068-lab-02-f20/src/components/Nav/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+// You're likely missing some imports...
+import {
+  BrowserRouter as Router, Link,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Home from '../Home';
+import About from '../About'
+import Styles from './styles';
+
+// Don't forget to import your components
+
+const Nav = () => {
+  return (
+    <Router>
+      <Styles.Nav>
+        <ul>
+          <li>
+             <Link to="/">Home</Link>         
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+        </ul>
+      </Styles.Nav>
+
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/About">
+          <About/>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default Nav;
